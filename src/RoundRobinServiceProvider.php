2826fd7dfa308498f72b35614cd2c699bbdc32da
boraguler@gmail.com <boraguler@gmail.com>
2020-04-02 20:30:49 +0300

    initial commits

--- src/RoundRobinServiceProvider.php
+++ src/RoundRobinServiceProvider.php
@@ -23,7 +23,7 @@ class RoundRobinServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->singleton('round-robin', function ($app) {
+        $this->app->singleton('RoundRobin', function ($app) {
             return new RoundRobin();
         });
     }
